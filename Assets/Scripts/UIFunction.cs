44ed94980b5c1f656ac04c901a7701acc1c1439e
shialian <50765318+shialian@users.noreply.github.com>
2021-07-20 11:00:29 +0800

    UI基本顯示
    在Playing Scene中，測試中

---
+++ Assets/Scripts/UIFunction.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFunction : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject previous;
+    public GameObject[] buttons;
+    public GameObject minimap;
+    public GameObject stamp;
+
+    private void Awake()
+    {
+        previous.SetActive(false);
+        HideButtons();
+        minimap.SetActive(false);
+        stamp.SetActive(false);
+    }
+
+    public void MenuClicked()
+    {
+        ShowAllButtons();
+        menu.SetActive(false);
+        previous.SetActive(true);
+    }
+
+    public void ShowMinimap()
+    {
+        minimap.SetActive(true);
+        HideButtons();
+    }
+
+    public void ShowStamp()
+    {
+        stamp.SetActive(true);
+        HideButtons();
+    }
+
+    public void BeckWard()
+    {
+        if(minimap.activeSelf)
+        {
+            ShowAllButtons();
+            minimap.SetActive(false);
+        }
+        else if (stamp.activeSelf)
+        {
+            ShowAllButtons();
+            stamp.SetActive(false);
+        }
+        else
+        {
+            HideButtons();
+            menu.SetActive(true);
+            previous.SetActive(false);
+        }
+    }
+
+    private void ShowAllButtons()
+    {
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].gameObject.SetActive(true);
+        }
+    }
+
+    private void HideButtons()
+    {
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].gameObject.SetActive(false);
+        }
+    }
+}
